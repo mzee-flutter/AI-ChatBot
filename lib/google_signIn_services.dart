74a7ce8014daa5de2881363eb0d1965949cd09fc
mzee-flutter <mudair.khan37@gmail.com>
2025-01-18 11:29:00 +0400

    this is google sign in implementation method

---
+++ lib/google_signIn_services.dart
@@ -0,0 +1,50 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'chat_page.dart';
+import 'shared_preferences_services.dart';
+import 'package:flutter/material.dart';
+
+class GoogleSignInServices {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final pref_services = SharedPreferencesServices();
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
+  Future<User?> SignInWithGoogle(context) async {
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      if (googleUser == null) {
+        return null;
+      }
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        accessToken: googleAuth.accessToken,
+        idToken: googleAuth.idToken,
+      );
+      final UserCredential userCredential =
+          await _auth.signInWithCredential(credential);
+      String? tokenID = await userCredential.user?.getIdToken();
+      if (tokenID != null) {
+        pref_services.saveToken(tokenID);
+        Navigator.pushNamedAndRemoveUntil(
+          context,
+          ChatPage.id,
+          (route) => false,
+        );
+      }
+      return userCredential.user;
+    } catch (e) {
+      if (kDebugMode) {
+        print(e.toString());
+      }
+    }
+    return null;
+  }
+
+  Future<void> SignOut() async {
+    await _auth.signOut();
+    await _googleSignIn.signOut();
+  }
+}
